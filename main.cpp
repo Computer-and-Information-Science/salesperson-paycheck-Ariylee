4c0f873af6f834884e88e35c0f10541795be9961
Ariyle Braswell <braswell.ariyle@gmail.com>
2025-09-16 23:27:44 +0000

    Sales Person Paycheck Assignment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
+//Ariyle Braswell
+//Salesperson Paycheck
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  string fileName;
+  cout << "Enter the input file name ";
+  cin >> fileName;
+
+  ifstream inFile(fileName);
+  if (!inFile) {
+  cerr << "Error opening file." <<endl;
+  return 1;
+}
+
+string firstName, lastName;
+double baseSalary, commissionPercent, totalSales, expenses;
+
+inFile >> firstName >> lastName;
+inFile >> baseSalary >> commissionPercent;
+inFile >> totalSales;
+inFile >> expenses;
+
+inFile.close();
+
+double commission = (commissionPercent / 100.0) * totalSales;
+double total = baseSalary + commission - expenses;
+
+cout << "Payroll data for " << firstName << " " << lastName << endl;
+cout << fixed << setprecision(2);
+
+cout << "\nBase Salary: " << setw(10) << baseSalary << endl;
+cout << "Commission: " << setw(10) << commission << " ( " << setprecision(1) << commissionPercent
+     << " % of " << fixed << setprecision(2) << totalSales << " ) " << endl;
+cout << "Expenses: " << setw(10) << expenses << endl;
+cout << "Total: " << setw(10) << total << endl;
+
+return 0;
 }
